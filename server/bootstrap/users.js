25aabdddb33bf58019fd1eb66c22306a056cefe1
Pranay92 <pranaydubey3009@gmail.com>
2015-06-14 15:26:00 +0530

    Use new file for user listing

---
+++ server/bootstrap/users.js
@@ -0,0 +1,56 @@
+module.exports = [{
+    name : {
+      'first' : 'Pranay',
+      'last' : 'Dubey'
+    },
+    dob : {
+      year : 1992,
+      month : 8,
+      date : 30
+    },
+    password : 'demo',
+    username : 'pranay@lets-chat.com',
+    type : 'Admin'
+  },
+  {
+    name : {
+      'first' : 'Pranay',
+      'last' : 'Dubey'
+    },
+    dob : {
+      year : 1992,
+      month : 9,
+      date : 30
+    },
+    password : 'demo',
+    username : 'pranay@lets-chat.com',
+    type : 'Admin'
+  },
+  {
+  	name : {
+  		'first' : 'Shamsher',
+  		'last' : 'Ansari'
+  	},
+  	dob : {
+  		year : 1989,
+  		month : 2,
+  		date : 31
+  	},
+  	password : 'demo',
+  	username : 'shamsher@lets-chat.com',
+  	type : 'Admin'
+  },
+  {
+  	name : {
+  		'first' : 'Amrutha',
+  		'last' : 'Rao'
+  	},
+  	dob : {
+  		year : 1989,
+  		month : 1,
+  		date  : 2
+  	},
+  	password : 'demo',
+  	username : 'amrutha@lets-chat.com',
+  	type : 'User'
+  }];
