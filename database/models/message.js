395cd72b8fd50cc969c7d4b34a2ecdb91fd26723
Pranay Dubey <pranaydubey3009@gmail.com>
2015-10-14 11:28:06 +0530

    Updates on message schema  - Add 'type' to diff between a message and notification

--- database/models/message.js
+++ database/models/message.js
@@ -2,22 +2,27 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var schema = {
-	from : {
-		type : Schema.ObjectId,
-		ref : 'User'
-	},
-	to : {
-		type : Schema.ObjectId,
-		ref : 'User'
-	},
-	content : {
-		type : String,
-		required : true
-	},
-	created : {
-		type : Number,
-		default : new Date().getTime()
-	}
+  from : {
+    type : Schema.ObjectId,
+    ref : 'User'
+  },
+  to : {
+    type : Schema.ObjectId,
+    ref : 'User'
+  },
+  content : {
+    type : String,
+    required : true
+  },
+  created : {
+    type : Number,
+    default : new Date().getTime()
+  },
+  type : {
+    type : String,
+    enum : ['notification','message'],
+    default : 'message'
+  }
 };
 
 var MongooseSchema = new mongoose.Schema(schema);
