e821bbb25cc9c33e7b18c1127cb49f430497cd2e
Pranay Dubey <pranaydubey3009@gmail.com>
2015-10-18 01:11:53 +0530

    Updates on chat server listeners  - Add listeners for personal messages  - Add listeners for group messages

--- utils/socket.js
+++ utils/socket.js
@@ -24,29 +24,57 @@ function Initialize(socket) {
 function Listeners() {
 
   var listeners = [
-          {
-            'event'   : 'message',
-            'handler' : Message
-          },
-          {
-            'event'   : 'group-message',
-            'handler' : GroupMessage
-          },
-          {
-            'event'   : 'group-add',
-            'handler' : GroupAdd
-
-          },{
-            'event'   : 'group-remove',
-            'handler' : GroupRemove
-          },
-          {
-            'event'   : 'group-create',
-            'handler' : GroupCreate
-          }
-        ];
+                    {
+                      'event'   : 'message-join', // subscribe to one-to-one chat
+                      'handler' : MessageJoin
+                    },
+                    {
+                      'event'   : 'message', // message event on one-to-one chat
+                      'handler' : Message
+                    },
+                    {
+                      'event'   : 'message-edit', // edit message event on one-to-one chat
+                      'handler' : MessageEdit
+                    },
+                    {
+                      'event'   : 'message-delete', // delete msg event on one-to-one chat
+                      'handler' : MessageDelete
+                    },
+                    {
+                      'event'   : 'group-create', // group create event
+                      'handler' : GroupCreate
+                    },
+                    {
+                      'event'   : 'group-join', // group join event
+                      'handler' : GroupJoin
+                    },
+                    {
+                      'event'   : 'group-message', // message event on a group chat
+                      'handler' : GroupMessage
+                    },
+                    {
+                      'event'   : 'group-message-edit', // edit message event on a group chat
+                      'handler' : GroupMessageEdit
+                    },
+                    {
+                      'event'   : 'group-message-delete', // edit message event on a group chat
+                      'handler' : GroupMessageDelete
+                    },
+                    {
+                      'event'   : 'group-delete', // group delete event
+                      'handler' : GroupDelete
+                    },
+                    {
+                      'event'   : 'group-left', // group leave event
+                      'handler' : GroupLeave
+                    }
+                  ];
 
   return listeners;
+
+};
+
+function MessageJoin(server) {
 
 };
 
@@ -54,21 +82,44 @@ function Message(server) {
 
 };
 
-function GroupMessage(server) {
+function MessageEdit(server) {
 
 };
 
-function GroupAdd(server) {
-
-};
-
-function GroupRemove(server) {
+function MessageDelete(server) {
 
 };
 
 function GroupCreate(server) {
 
 };
+
+function GroupJoin(server) {
+
+};
+
+function GroupMessage(server) {
+
+};
+
+function GroupMessageEdit(server) {
+
+};
+
+function GroupMessageDelete(server) {
+
+};
+
+function GroupLeave(server) {
+
+};
+
+function GroupDelete(server) {
+
+};
+
+
+
  
 
 
