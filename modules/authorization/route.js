86d34450e5e0268e24b86aadf23ca1182fd7f06e
Pranay Dubey <pranaydubey3009@gmail.com>
2015-11-16 06:10:12 +0530

    Update indentation to 2 spaces

--- modules/authorization/route.js
+++ modules/authorization/route.js
@@ -3,37 +3,37 @@ var Series = require('hapi-next'),
     Controller = require('modules/authorization/controller');
 
 module.exports = {
-	
-	login : {
-		method : 'POST',
-		path : '/login',
-		config : {
-			validate : Validator.validateReqLogin(),
-			handler : function(request,reply) {
+  
+  login : {
+    method : 'POST',
+    path : '/login',
+    config : {
+      validate : Validator.validateReqLogin(),
+      handler : function(request,reply) {
 
-				var series = new Series([
-					Validator.login,
-					Controller.login
-				]);
-				
-				series.execute(request,reply);
-			}
-		}
-	},
+        var series = new Series([
+          Validator.login,
+          Controller.login
+        ]);
+        
+        series.execute(request,reply);
+      }
+    }
+  },
 
-	logout : {
-		method : 'POST',
-		path : '/logout',
-		config : {
-			handler : function(request,reply) {
+  logout : {
+    method : 'POST',
+    path : '/logout',
+    config : {
+      handler : function(request,reply) {
 
-				var series = new Series([
-					Controller.logout
-				]);
-				
-				series.execute(request,reply);	
-			}
-		}
-	}
-	
+        var series = new Series([
+          Controller.logout
+        ]);
+        
+        series.execute(request,reply);  
+      }
+    }
+  }
+  
 }
